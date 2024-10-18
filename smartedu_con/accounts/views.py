a8093c1831f0e655a6c136f49ad2f10868e2600f
MetinQardasov11 <metin.qardasov2003@gmail.com>
2024-10-18 13:26:30 +0400

    Lesson 19

--- smartedu_con/accounts/views.py
+++ smartedu_con/accounts/views.py
@@ -1,16 +1,53 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import LoginForm, RegisterForm
 
 # Create your views here.
 
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration successful.')
+            return redirect('accounts:login')
+        else:
+            messages.error(request, 'Registration failed.')
+    else:
+        form = RegisterForm()
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'register.html', context)
+
 def login_view(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('pages:index')
+                else:
+                    messages.warning(request, 'Your account is not active.')
+            else:
+                messages.warning(request, 'Invalid login credentials.')
+    else:
+        form = LoginForm()
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'login.html', context)
 
 def logout_view(request):
-    return render(request, 'register.html')
-
-def register_view(request):
-    return render(request, 'register.html')
+    logout(request)
+    return redirect('pages:index')
 
 def dashboard_view(request):
-    pass
+    return render(request, 'dashboard.html')
